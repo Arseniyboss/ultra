fcc661272c2216cb18af2b3ed56b7c6f54de1fa4
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-02-21 14:25:27 +0100

    fix footer styles

--- src/components/footer/styles.tsx
+++ src/components/footer/styles.tsx
@@ -1,17 +1,22 @@
 'use client'
 
 import styled from 'styled-components'
+import { breakpoints } from '@breakpoints'
 
 export const SectionContainer = styled.section`
   display: flex;
   justify-content: center;
   column-gap: 5rem;
+  padding: 0 var(--spacing);
+
+  @media screen and (max-width: ${breakpoints.home}) {
+    margin-top: var(--spacing);
+  }
 
   @media screen and (max-width: 900px) {
     display: grid;
     grid-template-columns: repeat(2, auto);
     row-gap: var(--spacing);
-    margin-top: var(--spacing);
   }
 
   @media screen and (max-width: 500px) {
